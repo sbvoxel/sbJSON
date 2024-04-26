8d9673b6000d6baee18a3f335ed771432763b099
orri <orri@systemb.is>
2024-04-26 00:43:18 +0000

    clang format fuzz files

--- fuzzing/cjson_read_fuzzer.c
+++ fuzzing/cjson_read_fuzzer.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #ifdef __cplusplus
@@ -8,60 +8,62 @@ extern "C" {
 
 #include "../sbJSON.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size); /* required by C89 */
+int LLVMFuzzerTestOneInput(const uint8_t *data,
+                           size_t size); /* required by C89 */
 
-int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     sbJSON *json;
     size_t offset = 4;
     unsigned char *copied;
     char *printed_json = NULL;
     int minify, require_termination, formatted, buffered;
 
+    if (size <= offset)
+        return 0;
+    if (data[size - 1] != '\0')
+        return 0;
+    if (data[0] != '1' && data[0] != '0')
+        return 0;
+    if (data[1] != '1' && data[1] != '0')
+        return 0;
+    if (data[2] != '1' && data[2] != '0')
+        return 0;
+    if (data[3] != '1' && data[3] != '0')
+        return 0;
 
-    if(size <= offset) return 0;
-    if(data[size-1] != '\0') return 0;
-    if(data[0] != '1' && data[0] != '0') return 0;
-    if(data[1] != '1' && data[1] != '0') return 0;
-    if(data[2] != '1' && data[2] != '0') return 0;
-    if(data[3] != '1' && data[3] != '0') return 0;
-
-    minify              = data[0] == '1' ? 1 : 0;
+    minify = data[0] == '1' ? 1 : 0;
     require_termination = data[1] == '1' ? 1 : 0;
-    formatted           = data[2] == '1' ? 1 : 0;
-    buffered            = data[3] == '1' ? 1 : 0;
+    formatted = data[2] == '1' ? 1 : 0;
+    buffered = data[3] == '1' ? 1 : 0;
 
-    json = sbJSON_ParseWithOpts((const char*)data + offset, NULL, require_termination);
+    json = sbJSON_ParseWithOpts((const char *)data + offset, NULL,
+                                require_termination);
 
-    if(json == NULL) return 0;
+    if (json == NULL)
+        return 0;
 
-    if(buffered)
-    {
+    if (buffered) {
         printed_json = sbJSON_PrintBuffered(json, 1, formatted);
-    }
-    else
-    {
+    } else {
         /* unbuffered printing */
-        if(formatted)
-        {
+        if (formatted) {
             printed_json = sbJSON_Print(json);
-        }
-        else
-        {
+        } else {
             printed_json = sbJSON_PrintUnformatted(json);
         }
     }
 
-    if(printed_json != NULL) free(printed_json);
+    if (printed_json != NULL)
+        free(printed_json);
 
-    if(minify)
-    {
-        copied = (unsigned char*)malloc(size);
-        if(copied == NULL) return 0;
+    if (minify) {
+        copied = (unsigned char *)malloc(size);
+        if (copied == NULL)
+            return 0;
 
         memcpy(copied, data, size);
 
-        sbJSON_Minify((char*)copied + offset);
+        sbJSON_Minify((char *)copied + offset);
 
         free(copied);
     }
@@ -74,4 +76,3 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 #ifdef __cplusplus
 }
 #endif
-
