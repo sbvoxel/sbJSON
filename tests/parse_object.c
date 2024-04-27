d9f2e5cd70c2e9a20eb0997d3f27eebe87f77280
orri <orri@systemb.is>
2024-04-27 13:25:31 +0000

    Update copyright notices

--- tests/parse_object.c
+++ tests/parse_object.c
@@ -1,5 +1,6 @@
 /*
   Copyright (c) 2009-2017 Dave Gamble and cJSON contributors
+  Modifications (c) 2024 sbJSON Authors
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
